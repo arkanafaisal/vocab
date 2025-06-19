19925727b1fc775f443719100036fcd98f8b640e
arkanafaisal <arkana.faisal.r@gmail.com>
2025-06-19 23:57:07 +0700

    Add files via upload

---
+++ js/startFetching.js
@@ -0,0 +1,22 @@
+async function startFetching(api, method, token, body) {
+        try {
+            let options = {
+                method: method,
+                credentials:"include",
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+            if(token){options.headers.token = token}
+            if(body){options.body = JSON.stringify(body)}
+
+            const response = await fetch(api, options)
+
+
+            const result = await response.json()
+            return result
+        } catch(error) {
+            if(error.name === "TypeError"){return {success: false, message: "server mati, hubungi admin", data: null}}
+            return {success: false, message: "error, harap hubungi admin", data: null}
+        }
+    }
