a15622aa2fa3da9e44e8581f2dc307d7f6b90ef2
arkanafaisal <arkana.faisal.r@gmail.com>
2026-02-16 19:51:27 +0700

    email registration

--- backend/schema/user-schema.js
+++ backend/schema/user-schema.js
@@ -2,6 +2,7 @@ import Joi from 'joi'
 
 export const user = Joi.object({
     username: Joi.string().trim().max(15).pattern(/^[a-zA-Z0-9]+$/).required(),
+    email: Joi.string().email().trim().max(31),
     password: Joi.string().trim().max(255).required()
 })
 
