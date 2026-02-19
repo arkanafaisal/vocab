4440a4a3769bae7ff42259d9e71a271c82608a65
arkanafaisal <arkana.faisal.r@gmail.com>
2026-02-19 22:04:51 +0700

    port conflict

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend")))
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || (process.env.NODE_ENV === "development"? 3000 : 3002)
 
 
 import { setupSocket } from "./config/socket.js"
